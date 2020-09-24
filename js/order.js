918f6446dd8a223dd94842c22839843469093c00
3rdvision <kazgru@gmail.com>
2020-09-24 18:08:49 +0100

    Add hermes get order API to processor

--- js/order.js
+++ js/order.js
@@ -1,7 +1,7 @@
 export const OrderAPI = superclass =>
     class extends superclass {
-        async getOrder(number) {
-            const url = this.baseUrl + `orders/${number}`;
+        async getOrder(number, mapper) {
+            const url = this.baseUrl + `orders/${number}?mapper=${mapper}`;
             const contents = await this.get(url);
             return contents;
         }
