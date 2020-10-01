0f803a613b65dac7fc2a2cd514cd1586d42c4ee5
3rdvision <kazgru@gmail.com>
2020-10-01 16:50:32 +0100

    Adapt example

--- examples/simple.js
+++ examples/simple.js
@@ -1,10 +1,16 @@
 const epirProcessor = require("..");
 
+const baseUrl = "http://epir-processor-now.platforme.com/api/v1/";
+const token = process.env.RIPE_CORE_TOKEN;
+
 async function run() {
     await epirProcessor.API.load();
-    const api = new epirProcessor.API();
+    const api = new epirProcessor.API({ baseUrl: baseUrl });
+    const options = {
+        token: token
+    };
     try {
-        console.info(await api.info());
+        console.info(await api.getOrder(5555, options));
     } catch (err) {
         console.error(`[ERROR] ${err.code || ""} ${err.message}`);
     }
