8c426baf3265d3093fd3dadfa347d4b9b83fdb63
3rdvision <kazgru@gmail.com>
2020-10-02 11:14:25 +0100

    Receiving options

--- js/order.js
+++ js/order.js
@@ -2,12 +2,7 @@ export const OrderAPI = superclass =>
     class extends superclass {
         async getOrder(number, options = {}) {
             const url = this.baseUrl + `orders/${number}`;
-            options = {
-                headers: {
-                    "X-Secret-Key": options.token
-                }
-            };
-            const contents = await this.get(url, options);
+            const contents = await this.get(url, { auth: true, ...options });
             return contents;
         }
     };
